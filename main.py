1d55df423c498850667cc2a7304f835bee35a8cf
Victor Fernando <32652735+vfcv@users.noreply.github.com>
2022-03-10 10:28:46 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+#ETE PORTO DIGITAL
+#DISCIPLINA: LPC1
+#ALUNO: VICTOR FERNANDO CAMARA VIANA
+
+acao = 0
+while acao != 1:
+  print('\nComandos de controle de um aeromodelo:')
+  print('1-Acelerar')
+  print('2-Decolar')
+  print('3-Planar')
+  print('4-Piloto Automático')
+  print('5-Pousar')
+
+  acao = int(input('Digite a ação desejada:'))
+  
+  if acao == 1:
+    print("\nComando escolhido: Acelerar...")
+    print("Muito bem, agora vamos para o promixo comando!")
+  else: 
+    print("\nErro no comando! Siga a ordem correta para nao ter problemas em seu voo.")
+  
+
+while acao != 2:
+  print('\nComandos de controle de um aeromodelo:')
+  print('1-Acelerar')
+  print('2-Decolar')
+  print('3-Planar')
+  print('4-Piloto Automático')
+  print('5-Pousar')
+
+  acao = int(input('Digite a ação desejada:'))
+
+  if acao == 2:
+    print('\nComando escolhido: Decolar...')
+    print('Muito bem, agora vamos para o promixo comando!')
+  else: 
+    print('\nErro no comando! Siga a ordem correta para nao ter problemas em seu voo.')
+
+while acao != 3:
+  print('\nComandos de controle de um aeromodelo:')
+  print('1-Acelerar')
+  print('2-Decolar')
+  print('3-Planar')
+  print('4-Piloto Automático')
+  print('5-Pousar')
+
+  acao = int(input('Digite a ação desejada:'))
+
+  if acao == 3:
+    print('\nComando escolhido: Planar...')
+    print('Muito bem, agora vamos para o promixo comando!')
+  else: 
+    print('\nErro no comando! Siga a ordem correta para nao ter problemas em seu voo.')
+
+while acao != 4:
+  print('\nComandos de controle de um aeromodelo:')
+  print('1-Acelerar')
+  print('2-Decolar')
+  print('3-Planar')
+  print('4-Piloto Automático')
+  print('5-Pousar')
+
+  acao = int(input('Digite a ação desejada:'))
+
+  if acao == 4:
+    print('\nComando escolhido: Piloto Automático...')
+    print('Muito bem, agora vamos para o promixo comando!')
+  else: 
+    print('\nErro no comando! Siga a ordem correta para nao ter problemas em seu voo.')
+
+while acao != 5:
+  print('\nComandos de controle de um aeromodelo:')
+  print('1-Acelerar')
+  print('2-Decolar')
+  print('3-Planar')
+  print('4-Piloto Automático')
+  print('5-Pousar')
+
+  acao = int(input('Digite a ação desejada:'))
+
+  if acao == 5:
+    print('\nComando escolhido: Pousar...')
+    print('Muito bem, você concluiu seu voo! Parabens!!')
+  else: 
+    print('\nErro no comando! Siga a ordem correta para nao ter problemas em seu voo.')
